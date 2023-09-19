e481988720ec5269fe4d14e73dd5f6e944741ebe
Wolfhill09 <harvey.akhurst@icloud.com>
2023-09-19 14:05:49 +0100

    Tweak(Manifest): Update resource lua to be an fxmanifest

--- vRPStaffOn/fxmanifest.lua
+++ vRPStaffOn/fxmanifest.lua
@@ -1,16 +1,14 @@
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-
-author "Shaz"
-
-
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Harv'
 client_scripts {
 	"@vrp/client/Tunnel.lua",
     "@vrp/client/Proxy.lua",
 	'client/main.lua',
 }
 
-
 server_scripts {
+
 	"@vrp/lib/utils.lua",
 	'server/main.lua',
 }
