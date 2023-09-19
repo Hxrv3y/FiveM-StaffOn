87d0fa81d3cbd47c232503ba9d5d3706c1a92f0b
Wolfhill09 <harvey.akhurst@icloud.com>
2023-09-19 14:08:03 +0100

    Tweak(vRP Serverside): Add staffon and off command

--- vRPStaffOn/server/main.lua
+++ vRPStaffOn/server/main.lua
@@ -4,23 +4,19 @@ Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP", "vrp_staffmode")
 
---[[
-
-For other scripts do TriggerClientEvent("vRP:staffon", source, true) for them to staff on and TriggerClientEvent("vRP:staffon", source, false) for them to staff off
-    
-]]
-
 local staffmode = {}
 
-RegisterCommand("staffmode", function(source, args)
+RegisterCommand("staffon", function(source, args)
 	local user_id = vRP.getUserId({source})
 	if vRP.hasPermission({user_id, "staff.mode"}) then 
 		if not staffmode[source] then 
 			TriggerClientEvent("vRP:staffon", source, true)
 			staffmode[source] = source
-		else
-			TriggerClientEvent("vRP:staffon", source, false)
-			staffmode[source] = nil
 		end
 	end
+end)
+
+RegisterCommand("staffoff", function(source, args)
+	TriggerClientEvent("vRP:staffon", source, false)
+	staffmode[source] = nil
 end)
